6b20b80ea490ab737cc49324ad678b7fe924c712
her0e1c1 <her0e1c1@gmail.com>
2013-04-05 00:30:13 +0900

    add .gitignore in home directory

--- init/git.sh
+++ init/git.sh
@@ -9,4 +9,4 @@ git config --global core.editor vi
 git config --global user.name "her0e1c1"
 git config --global user.email her0e1c1@gmail.com
 git config --global color.ui true
-
+git config --global core.excludesfile ~/.gitignore
