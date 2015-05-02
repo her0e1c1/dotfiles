2e7e3f7b1703f4c338b2fd047af726d835a41c49
her0e1c1 <her0e1c1@gmail.com>
2015-05-02 14:19:05 +0000

    auto commit by fabric

--- sh/mac.sh
+++ sh/mac.sh
@@ -2,5 +2,7 @@ if uname -a | grep -qv Darwin; then
    return 0;
 fi   
 
+alias opens="open ~/Dropbox/sphinx/_build/index.html"
+alias openp=" ~/Dropbox/diary/output/index.html"
 alias emacs="/Applications/Emacs.app/Contents/MacOS/Emacs"
 alias emacsclient="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient"
