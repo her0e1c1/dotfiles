3e4a9297d529e6322131bb918e552a0836410982
her0e1c1 <her0e1c1@gmail.com>
2015-07-05 22:24:23 +0900

    auto commit by fabric

--- import.hs
+++ import.hs
@@ -1,7 +1,9 @@
+import GHC.Base
 import Data.Char
 import Data.List
 import Data.STRef
 import Text.Parsec
 import Control.Monad
+import qualified Control.Monad.State as S
 import Control.Monad.ST
 import Control.Applicative
