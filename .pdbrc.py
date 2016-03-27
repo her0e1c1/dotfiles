750916d8489f63d92e5abbe81d8c173aa6019236
her0e1c1 <her0e1c1@gmail.com>
2016-03-27 06:30:44 +0000

    add .pdbrc.py

---
+++ .pdbrc.py
@@ -0,0 +1,15 @@
+prompt = '(Pdb++) '
+highlight = True
+encoding = 'utf-8'
+sticky_by_default = True
+# line_number_color = Color.turquoise
+# filename_color = Color.yellow
+current_line_color = 44
+use_pygments = True
+bg = 'dark'
+colorscheme = None
+editor = '${EDITOR:-vi}'
+truncate_long_lines = True
+exec_if_unfocused = None
+disable_pytest_capturing = True
+def setup(self, pdb): pass
