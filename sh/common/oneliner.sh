1668f10e8cf71a945fdc7805db3d55092cfd875e
her0e1c1 <her0e1c1@gmail.com>
2016-04-30 14:47:13 +0000

    up

--- sh/common/oneliner.sh
+++ sh/common/oneliner.sh
@@ -1,6 +1,10 @@
+# chomp(@a=<stdin>)
 
 # Show all variables in mysql. Trim each line
 # mysql db -e 'show variables;' | perl_mysql_parse_variables
 perl_mysql_parse_variables() {
-    perl -nlE '@a=(split " ", $_); $k=$a[1]; $v=$a[3]||1; $v=="|" && ($v=1); say "$k=$v" if $k && 4 .. ((eof) - 1)'
+    # Get key = value lines, trime each line and show key = val
+    perl -lE '@a=<stdin>; say @a[4..$#a-1]'|
+    perl -nlE 'say split " "' | 
+    perl -nlE '@a=split "\\|"; say "$a[1] = $a[2]"'
 }
