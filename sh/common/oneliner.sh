a0a0c35e5df2c9b9e2c64f4626d60b2249416c0e
her0e1c1 <her0e1c1@gmail.com>
2016-04-30 15:24:27 +0000

    add onelienr

--- sh/common/oneliner.sh
+++ sh/common/oneliner.sh
@@ -1,5 +1,18 @@
 # chomp(@a=<stdin>)
 
+perl_to_upper() {perl -plE "tr/a-z/A-Z/"}
+perl_to_lower() {perl -plE "tr/A-Z/a-z/"}
+perl_get_image_tags() {curl $1 2> /dev/null | perl -nlE 'say $& if /<img.*?(gif|png|jpg).*?>/'}
+
+perl_duplicated_words() {perl -0777 -ne 'print qq/$.: $_/ while /\b(\w+)\b\s+\b\1\b/gi'}
+
+perl_filter_line() {
+    # のファイルについて、.jsを含む行をファイル名と合わせて表示. なお、$ARGVでファイル名表示"
+    local s='say qq/$ARGV $_/'
+    s="$s if /$1/"
+    xargs perl -nlE "'$s'"
+}
+
 # Show all variables in mysql. Trim each line
 # mysql db -e 'show variables;' | perl_mysql_parse_variables
 perl_mysql_parse_variables() {
@@ -7,4 +20,11 @@ perl_mysql_parse_variables() {
     perl -lE '@a=<stdin>; say @a[4..$#a-1]'|
     perl -nlE 'say split " "' | 
     perl -nlE '@a=split "\\|"; say "$a[1] = $a[2]"'
+}
+
+perl_parse_single_quote() {
+    # 'STRING' のようなクオートの文字列を抜き出す
+    # echo "this is a 'HOGE' hoge 'FOO' " |perl -nlE "while(m/'(.*?)'/g){say $+;}"
+    # gをつけることで$_の開始位置が巡る
+    perl -nlE "while(m/'(.*?)'/g){say $+;}"
 }
