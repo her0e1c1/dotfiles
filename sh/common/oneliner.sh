ceb62c5ac081756882bd75a89970977b39b9dd6a
her0e1c1 <her0e1c1@gmail.com>
2016-05-01 07:01:31 +0000

    add commands

--- sh/common/oneliner.sh
+++ sh/common/oneliner.sh
@@ -1,8 +1,17 @@
 # chomp(@a=<stdin>)
 
+# NETA
+# 
+# fizz buzz
+# 単項プラス演算子
+# perl -E 'say +(Fizz)[$_%3].(Buzz)[$_%5]||$_ for 1..100'"
+# Hello world in C
+# echo '#include <stdio.h>\nint main(){printf(\"hello world\");} ' |clang -x c - -o a.out && ./a.out")
+
 perl_to_upper() {perl -plE "tr/a-z/A-Z/"}
 perl_to_lower() {perl -plE "tr/A-Z/a-z/"}
 perl_get_image_tags() {curl $1 2> /dev/null | perl -nlE 'say $& if /<img.*?(gif|png|jpg).*?>/'}
+perl_color() {perl -E 'print qq/\x1b[38;5;${_}mC$_ / for 0..255; say'}
 
 perl_duplicated_words() {perl -0777 -ne 'print qq/$.: $_/ while /\b(\w+)\b\s+\b\1\b/gi'}
 
@@ -28,3 +37,12 @@ perl_parse_single_quote() {
     # gをつけることで$_の開始位置が巡る
     perl -nlE "while(m/'(.*?)'/g){say $+;}"
 }
+
+# FILEPATH
+perl_insert_first_line() { xargs perl -i -plE "say qq/$1/ if 1..1; \$.=0 if eof" }
+
+# sync selected directories in the two directories
+# ; perl -E 'for ("css", "fonts", "js"){ system "rsync -avz --delete src/html/$_ dest/public/" }'
+
+db_filter_insert_into() { perl -nlE 'say if /^INSERT INTO/' }
+db_psql_dump_url() {docker run --rm -it postgres:9.5 pg_dump --data-only --column-inserts -d $1}
