f0a7ec27dc342f7d395112b458e032e8695f836e
her0e1c1 <her0e1c1@gmail.com>
2023-08-13 12:25:21 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-echo "try to run install.sh"
+echo "try to run install.sh　at $(pwd)"
 
 if [ "$CODESPACES" ==  true ]; then
   sudo apt-get update -y
   sudo apt-get install -y tig peco;
+  for file in .*; do
+        if [ -f $file ]; then
+            ln -sf $file ~/$file
+        fi
+    done
 fi
